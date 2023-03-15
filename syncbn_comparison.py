8a8d92df8f07173f02fd51bc7f7c167a02c00799
Dmitry Lishudi <lishudi.dmitry@gmail.com>
2023-03-15 06:41:13 +0300

    minor fix

--- syncbn_comparison.py
+++ syncbn_comparison.py
@@ -16,13 +16,11 @@ from time import perf_counter
 torch.set_num_threads(1)
 
 
-def init_process(local_rank, fn, device_type="cpu", bn="no"):
+def init_process(local_rank, bn="no"):
     """Initialize the distributed environment."""
-    backend = "gloo" if device_type == "cpu" else "nccl"
-    device = "cpu" if device_type == "cpu" else torch.device("cuda", local_rank)
-    dist.init_process_group(backend, rank=local_rank)
+    dist.init_process_group("gloo", rank=local_rank)
     size = dist.get_world_size()
-    run_experiments(local_rank, size, device)
+    run_experiments(local_rank, bn)
 
 
 # added scale: we need to divide by grad_accum_steps
@@ -37,10 +35,15 @@ def average_gradients(model, scale=1):
 def get_atol_rtol_time(local_rank, batch_size, n_features, bn):
     torch.manual_seed(0)
     custom_bn = SyncBatchNorm(n_features)
-    reference_bn = nn.SyncBatchNorm(n_features, affine=False)
+    reference_bn = nn.SyncBatchNorm(n_features, affine=False) if bn == "sync" else nn.BatchNorm1d(n_features, affine=False)
     torch.manual_seed(local_rank)
-    data = torch.randn(batch_size, n_features)
-    pass
+    data = torch.randn(batch_size//2, n_features)
+
+    custom_forward = custom_bn(data)
+    reference_forward = reference_bn(data)
+    custom_backward = custom_forward.sum().backward()
+    reference_backward = reference_forward.sum().backward()
+
 
 
 def run_experiments(local_rank, bn):
@@ -55,4 +58,4 @@ if __name__ == "__main__":
     parser.add_argument('-d', '--device', type=str, nargs='?', default="cpu")
     parser.add_argument('-b', '--batchnorm', type=str, nargs='?', default="sync")
     args = parser.parse_args()
-    init_process(local_rank, fn=run_training, device_type=args.device, bn=args.batchnorm)
+    init_process(local_rank, fn=run_training, bn=args.batchnorm)
